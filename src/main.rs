617a035d509e50aebcae73078dafcc80d4016456
gwo0d <git@gwood.dev>
2023-11-14 13:55:26 +0000

    Implement gen_iv function in main.rs. - Implemented gen_iv function to generate a pseudorandom IV for use in encryption. - Added rand dependency to facilitate pseudorandom number generation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use sha3::{Shake128, digest::{Update, ExtendableOutput, XofReader}};
+use rand::Rng;
 
 fn main() {
-    derive_key("hello world".to_string());
+    println!("Hello, world!");
 }
 
 fn derive_key(input_str: String) -> [u8; 16] {
@@ -12,4 +13,12 @@ fn derive_key(input_str: String) -> [u8; 16] {
     reader.read(&mut key);
 
     return key;
+}
+
+fn gen_iv() -> [u8; 16] {
+    let mut rng = rand::thread_rng();
+    let mut iv: [u8; 16] = [0; 16];
+    rng.fill(&mut iv);
+
+    return iv;
 }
