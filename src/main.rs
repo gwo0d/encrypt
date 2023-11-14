113ed065a8cf3fc7ff69e08d89400c7204275071
gwo0d <git@gwood.dev>
2023-11-14 13:42:40 +0000

    Implement derive_key function in main.rs: - Added derive_key function which returns a 128-bit hexadecimal array based on the SHAKE hash of the input_str parameter. - Imported sha3 module, which is used in the derive_key function above.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use sha3::{Shake128, digest::{Update, ExtendableOutput, XofReader}};
+
 fn main() {
-    println!("Hello, world!");
+    derive_key("hello world".to_string());
+}
+
+fn derive_key(input_str: String) -> [u8; 16] {
+    let mut hasher = Shake128::default();
+    hasher.update(input_str.as_bytes());
+    let mut reader = hasher.finalize_xof();
+    let mut key: [u8; 16] = [0; 16];
+    reader.read(&mut key);
+
+    return key;
 }
